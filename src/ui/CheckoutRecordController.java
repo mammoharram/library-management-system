664e4f8a8c7839f40ea40c9aa1c72c99b6a86c6d
Yusuf Hussein <yusuf.hussein8894@gmail.com>
2019-06-06 16:28:53 -0500

    Add show overdue books

--- src/ui/CheckoutRecordController.java
+++ src/ui/CheckoutRecordController.java
@@ -14,8 +14,10 @@ import dataaccess.DataAccessFacade;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -23,6 +25,8 @@ import javafx.scene.control.TextField;
 import javafx.util.Pair;
 
 public class CheckoutRecordController {
+	@FXML
+	private TextField isbnTextField;
 	@FXML
 	private TableView<Pair<LibraryMember, CheckoutEntry>> checkoutRecordTable;
 	@FXML
@@ -35,10 +39,14 @@ public class CheckoutRecordController {
 	private TableColumn<Pair<LibraryMember, CheckoutEntry>, String> checkoutDateCol;
 	@FXML
 	private TableColumn<Pair<LibraryMember, CheckoutEntry>, String> dueCol;
+	@FXML
+	private TableColumn<Pair<LibraryMember, CheckoutEntry>, String> isOverDue;
+	@FXML
+	private CheckBox showOverDueCheckBox;
 
 	@FXML
 	private void initialize() {
-		loadData();
+		loadData(null);
 		initializeView();
 	}
 
@@ -53,9 +61,11 @@ public class CheckoutRecordController {
 				cellData -> new SimpleStringProperty(cellData.getValue().getValue().getCheckoutDate().toString()));
 		dueCol.setCellValueFactory(
 				cellData -> new SimpleStringProperty(cellData.getValue().getValue().getDueDate().toString()));
+		isOverDue.setCellValueFactory(cellData -> new SimpleStringProperty(
+				new Boolean(cellData.getValue().getValue().getDueDate().before(new Date())).toString()));
 	}
 
-	private void loadData() {
+	private void loadData(String isbnFilter) {
 
 		DataAccess da = new DataAccessFacade();
 		List<LibraryMember> memberList = new ArrayList<LibraryMember>(da.readMemberMap().values());
@@ -63,12 +73,29 @@ public class CheckoutRecordController {
 		for (LibraryMember member : memberList) {
 			List<CheckoutEntry> checkoutEntries = member.getCheckoutRecord().getCheckoutEntries();
 			for (CheckoutEntry checkoutEntry : checkoutEntries) {
-				membersAndCheckoutEntryList.add(new Pair<LibraryMember, CheckoutEntry>(member, checkoutEntry));
+				if (isbnFilter == null ||checkoutEntry.getCheckedoutBookCopy().getBook().getIsbn().contains(isbnFilter)) {
+					if (showOverDueCheckBox.isSelected()) {
+						if (checkoutEntry.getDueDate().before(new Date())) {
+							membersAndCheckoutEntryList
+									.add(new Pair<LibraryMember, CheckoutEntry>(member, checkoutEntry));
+						}
+					} else {
+						membersAndCheckoutEntryList.add(new Pair<LibraryMember, CheckoutEntry>(member, checkoutEntry));
+					}
+				}
 			}
 		}
 		ObservableList<Pair<LibraryMember, CheckoutEntry>> membersAndCheckoutEntries = FXCollections
 				.observableArrayList(membersAndCheckoutEntryList);
 		checkoutRecordTable.setItems(membersAndCheckoutEntries);
 
+	}
+
+	public void OnIsbnTextFieldChanged(ActionEvent event) {
+		loadData(isbnTextField.getText());
+	}
+
+	public void onChangeOverdueCheckBox(ActionEvent event) {
+		loadData(isbnTextField.getText());
 	}
 }
