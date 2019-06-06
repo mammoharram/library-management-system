b5fd658469c2060efc1c8c0999cfb1752b0c82f2
Mohammad Moharram <mammoharram@hotmail.com>
2019-06-06 13:44:43 -0500

    Implement Add Edit List Members with some validations

--- src/business/LibraryMember.java
+++ src/business/LibraryMember.java
@@ -9,24 +9,31 @@ import dataaccess.DataAccessFacade;
 
 final public class LibraryMember extends Person implements Serializable {
 	private String memberId;
-	
+
+	public LibraryMember() {
+		super();
+	}
 	public LibraryMember(String memberId, String fname, String lname, String tel,Address add) {
 		super(fname,lname, tel, add);
-		this.memberId = memberId;		
+		this.memberId = memberId;
 	}
-	
-	
+
+
 	public String getMemberId() {
 		return memberId;
 	}
 
-	
-	
+
+
 	@Override
 	public String toString() {
-		return "Member Info: " + "ID: " + memberId + ", name: " + getFirstName() + " " + getLastName() + 
+		return "Member Info: " + "ID: " + memberId + ", name: " + getFirstName() + " " + getLastName() +
 				", " + getTelephone() + " " + getAddress();
 	}
 
 	private static final long serialVersionUID = -2226197306790714013L;
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
 }
--- src/business/SystemController.java
+++ src/business/SystemController.java
@@ -9,10 +9,12 @@ import dataaccess.Auth;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessFacade;
 import dataaccess.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class SystemController implements ControllerInterface {
 	public static Auth currentAuth = null;
-	
+
 	public void login(String id, String password) throws LoginException {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, User> map = da.readUserMap();
@@ -24,7 +26,7 @@ public class SystemController implements ControllerInterface {
 			throw new LoginException("Password incorrect");
 		}
 		currentAuth = map.get(id).getAuthorization();
-		
+
 	}
 	@Override
 	public List<String> allMemberIds() {
@@ -33,7 +35,16 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readMemberMap().keySet());
 		return retval;
 	}
-	
+
+	public ObservableList<LibraryMember> getAllLibraryMember() {
+		// TODO Auto-generated method stub
+		ObservableList<LibraryMember> MemberData = FXCollections.observableArrayList();
+		DataAccessFacade da = new DataAccessFacade();
+		MemberData.clear();
+		MemberData.addAll(da.readMemberMap().values());
+		return MemberData;
+	}
+
 	@Override
 	public List<String> allBookIds() {
 		DataAccess da = new DataAccessFacade();
@@ -41,6 +52,40 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readBooksMap().keySet());
 		return retval;
 	}
-	
-	
+	public void addMember(LibraryMember libMem) throws Exception {
+		DataAccessFacade da = new DataAccessFacade();
+		ArrayList<LibraryMember> memberList = new ArrayList<LibraryMember>();
+
+		memberList.addAll(da.readMemberMap().values());
+		if (memberList.contains(libMem)) {
+			int index = memberList.indexOf(libMem);
+			memberList.set(index, libMem);
+			DataAccessFacade.loadMemberMap(memberList);
+		}
+		else {
+		LibraryMember lastMember = 	memberList.get(memberList.size()-1);
+		Integer lastId = 0;
+		if(lastMember!=null)
+			lastId =new Integer(lastMember.getMemberId())+1;
+
+		libMem.setMemberId(lastId.toString());
+		da.saveNewMember(libMem);
+		}
+	}
+//	public void addAdress(LibraryMember libMem) throws Exception {
+//		DataAccessFacade da = new DataAccessFacade();
+//		//ArrayList<LibraryMember> memberList = new ArrayList<LibraryMember>();
+//
+//		memberList.addAll(da.readMemberMap().values());
+//		if (memberList.contains(libMem)) {
+//			int index = memberList.indexOf(libMem);
+//			memberList.set(index, libMem);
+//			DataAccessFacade.loadMemberMap(memberList);
+//		}
+//		else {
+//			da.saveNewMember(libMem);
+//		}
+//	}
+
+
 }
